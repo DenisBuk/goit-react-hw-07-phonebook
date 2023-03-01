5240a85d5fb73ee0c73076b33437fac239201472
DenisBuk <79395480+DenisBuk@users.noreply.github.com>
2023-03-01 14:35:39 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-import { App } from 'components/App';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import  App  from 'components/App';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root');
+const root = createRoot(container)
+  root.render(
   <React.StrictMode>
-    <Provider store={store}>
-    <BrowserRouter basename="goit-react-hw-07-phonebook">
-  <App />
-      </BrowserRouter>
-      </Provider>
+      <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>
 );
