c3aa40fb0a8c643d4fd3b62f2d3f928c58e739ec
DenisBuk <79395480+DenisBuk@users.noreply.github.com>
2023-03-01 11:54:48 +0200

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import css from "./App.module.css";
 
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 import ContactForm from "./components/ContactForm";
 import Filter from "./components/Filter/Filter";
 import ContactsList from "./components/ContactList";
 import Container from "./components/Container";
+import { ChakraProvider, Heading } from "@chakra-ui/react";
 
 function App(){
-    
-    return (
+  return (
+      <ChakraProvider>
       <div className={css.App}>
       <Container>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
+          <Heading mb={5}>Phonebook</Heading>
+          <ContactForm />
+           <Heading mb={5}>Contacts</Heading>
       <Filter />
       <ContactsList />
-      </Container>
+        </Container>
+        <ToastContainer
+          autoClose={2000}
+          position="top-right"
+          hideProgressBar={true}
+        />
       </div>
+      </ChakraProvider>
     );
 }
 
